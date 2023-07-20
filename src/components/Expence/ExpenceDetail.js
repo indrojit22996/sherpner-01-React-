987a57022213096f6fbc2809ad64ad0ec46a252b
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-07-20 19:11:22 +0530

    Add eventLisner and Delete Expence

--- src/components/Expence/ExpenceDetail.js
+++ src/components/Expence/ExpenceDetail.js
@@ -1,9 +1,14 @@
 function ExpenseDetail({obj}) {
+  // const 
+  function handelDeleteExpense(){
+    document.querySelector(".expense-item__description").remove();
+  }
   return (
     <div className="expense-item__description">
       <h1>{obj.title}</h1>
       <h2>{obj.LocationOfExpenditure}</h2>
-      <div className="expense-item__price">{obj.amount}</div>
+      <div className="expense-item__price">{obj.amount}</div> 
+      <button onClick={handelDeleteExpense}>Delete Expense</button>
     </div>
   );
 }
