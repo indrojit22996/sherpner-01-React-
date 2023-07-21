cdeb72c282f0143950bec5dfca713f3e93851c04
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-07-21 19:52:02 +0530

    useState Use

--- src/components/Expence/ExpenceDetail.js
+++ src/components/Expence/ExpenceDetail.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 function ExpenseDetail({obj}) {
   // const 
-  function handelDeleteExpense(){
-    document.querySelector(".expense-item__description").remove();
+
+  const [title,setTitle]=useState(obj.title);
+  const [expense ,setExpense ]=useState(obj.amount);
+  // function handelDeleteExpense(){
+  //   document.querySelector(".expense-item__description").remove();
+  // }
+  function updateExpense(){
+    setExpense("$100")
+  }
+
+  function changeTitle(){
+    setTitle("Updated!!!")
   }
   return (
     <div className="expense-item__description">
-      <h1>{obj.title}</h1>
+      <h1>{title}</h1>
       <h2>{obj.LocationOfExpenditure}</h2>
-      <div className="expense-item__price">{obj.amount}</div> 
-      <button onClick={handelDeleteExpense}>Delete Expense</button>
-    </div>
+      <div className="expense-item__price">{expense}</div> 
+      <button onClick={updateExpense}>Update Expense </button>
+      {/*<button onClick={handelDeleteExpense}>Delete Expense</button>*/}
+      <button onClick={changeTitle}>Change Title </button>
+      </div>
   );
 }
 export default ExpenseDetail;
