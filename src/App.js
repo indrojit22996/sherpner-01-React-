03d0ee6ad2f9c5a2ce6954ccf7a5de3f86361441
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-07-24 20:45:39 +0530

    expense tracker

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { expenseData } from "./data";
 import "./components/Expence/ExpenseItem.css";
 import Card from "./components/UI/Card";
 import { NewExpense } from "./components/NewExpence/NewExpense";
+import { useState } from "react";
+// let newExpenceData = expenseData;
+
 const App = () => {
+
+const[userData,setUserData]=useState(expenseData)
+
 function handelExpenceData(expence){
   console.log('App.js');
   console.log(expence)
+  setUserData([expence,...expenseData]);
+  
 }
 
-  const newExpenceData = expenseData;
+
+   
   return (
     <div>
       <NewExpense onAddExpence={handelExpenceData} />
       <Card className="expence">
-        {newExpenceData.map((exdata) => (
+        {userData.map((exdata) => (
           <ExpenseItems expenceObj={exdata} key={exdata.id} />
         ))}
       </Card>
