1c75a0fffbadf642adc13cfc3e366e130072d791
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-07-25 19:10:49 +0530

    Implementing the filter feature

--- src/App.js
+++ src/App.js
@@ -4,28 +4,36 @@ import "./components/Expence/ExpenseItem.css";
 import Card from "./components/UI/Card";
 import { NewExpense } from "./components/NewExpence/NewExpense";
 import { useState } from "react";
-import ExpensesFilter from './components/Expence/ExpensesFilter'
+import ExpensesFilter from "./components/Expence/ExpensesFilter";
 // let newExpenceData = expenseData;
 
-const App = () => {
+const App = (props) => {
+  const [userData, setUserData] = useState(expenseData);
 
-const[userData,setUserData]=useState(expenseData)
+  function handelExpenceData(expence) {
+    console.log("App.js");
+    console.log(expence);
+    setUserData([expence, ...expenseData]);
+  }
+  const [filteredyear, setFilterdYear] = useState("2020");
+  function filterChangeHandeler(selectYear) {
+    setFilterdYear(selectYear);
+  }
 
-function handelExpenceData(expence){
-  console.log('App.js');
-  console.log(expence)
-  setUserData([expence,...expenseData]);
-  
-}
+  const filteredExpence = props.item.filter((expence) => {
+    return expence.date.getFullYear().toString() === filteredyear;
+  });
 
-
-   
+  console.log(filteredExpence);
   return (
     <div>
       <NewExpense onAddExpence={handelExpenceData} />
       <Card className="expence">
-      <ExpensesFilter />
-        {userData.map((exdata) => (
+        <ExpensesFilter
+          selected={filteredyear}
+          onChangeFilter={filterChangeHandeler}
+        />
+        {filteredExpence.map((exdata) => (
           <ExpenseItems expenceObj={exdata} key={exdata.id} />
         ))}
       </Card>
